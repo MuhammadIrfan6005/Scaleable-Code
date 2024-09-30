68ad20734cfbd4f4c7619032b348b668ef6aaf4b
MuhammadIrfan6005 <55645278+MuhammadIrfan6005@users.noreply.github.com>
2024-09-30 16:19:45 +0500

    Add files via upload

---
+++ Scaleable.Xrm.Plugins.ActAccData.SyncActParties/createRetrieveActivityParties.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaleable.Xrm.Plugins.ActAccData.SyncActParties
+{
+    public class createRetrieveActivityParties : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingservice = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the organization service reference.
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is EntityReference && context.MessageName.ToUpper() == "DELETE")
+            {
+                return;
+            }
+
+            else if ((context.InputParameters.Contains("Target")) && (context.InputParameters["Target"] is Entity) && ((context.MessageName.ToUpper() == "UPDATE") || (context.MessageName.ToUpper() == "CREATE")))
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if (entity.LogicalName != "ss_activitiesaccountdata")
+                {
+                    return;
+                }
+                if (context.Depth > 2)
+                {
+                    return;
+                }
+
+                try
+                {
+                    string activityId = null;
+                    Entity partiesImage = new Entity();
+
+                    if (entity.Attributes.Contains("ss_activityid"))
+                    {
+                        if (entity["ss_activityid"] != null)
+                        {
+                            activityId = (string)(entity["ss_activityid"]);
+                        }
+                    }
+                    else if (context.MessageName.ToUpper() == "UPDATE")
+                    {
+                        partiesImage = (Entity)context.PreEntityImages["partiesImage"];
+
+
+                        if (partiesImage.Attributes.Contains("ss_activityid"))
+                        {
+                            activityId = partiesImage.GetAttributeValue<string>("ss_activityid");
+                        }
+                    }
+
+                    deleteActivityParties(service, tracingservice, activityId);
+
+                    var QEactivityparty = new QueryExpression("activityparty");
+                    QEactivityparty.ColumnSet.AddColumns("partyid", "activitypartyid", "ispartydeleted", "activityid");
+                    QEactivityparty.Criteria.AddCondition("activityid", ConditionOperator.Equal, activityId);
+                    //QEactivityparty.Criteria.AddCondition("participationtypemask", ConditionOperator.NotEqual, 8);
+                    EntityCollection activityParties = service.RetrieveMultiple(QEactivityparty);
+
+                    EntityReference party = new EntityReference();
+
+                    foreach (Entity activityparty in activityParties.Entities)
+                    {
+                        if (activityparty.Attributes.Contains("partyid") && activityparty["partyid"] != null)
+                        {
+
+                            party = (EntityReference)activityparty["partyid"];
+                            Entity activitiesAccountParties = new Entity("ss_activityaccountparties");
+                            activitiesAccountParties.Attributes["ss_activityid"] = activityId;
+                            activitiesAccountParties.Attributes["ss_partyid"] = party.Id.ToString();
+                            activitiesAccountParties.Attributes["ss_partylogicalname"] = party.LogicalName;
+
+                            EntityReference activityaccountdata = new EntityReference("ss_activitiesaccountdata");
+                            activityaccountdata.Id = entity.Id;
+
+                            activitiesAccountParties.Attributes["ss_activityaccountdata"] = activityaccountdata;
+
+                            try
+                            {
+                                service.Create(activitiesAccountParties);
+                            }
+                            catch (Exception e)
+                            {
+                                tracingservice.Trace(e.ToString());
+                            }
+
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in the Scalable.Xrm.Plugins.ActivitiesAccountData plug-in.", ex);
+                }
+            }
+        }
+
+        public void deleteActivityParties(IOrganizationService service, ITracingService tracingservice, string activityId)
+        {
+            var ss_activityaccountparties = new QueryExpression("ss_activityaccountparties");
+            ss_activityaccountparties.ColumnSet.AddColumns("ss_activityid");
+            ss_activityaccountparties.Criteria.AddCondition("ss_activityid", ConditionOperator.Equal, activityId);
+
+            EntityCollection partiescollection = service.RetrieveMultiple(ss_activityaccountparties);
+
+            foreach (Entity partyentity in partiescollection.Entities)
+            {
+                try
+                {
+                    service.Delete(partyentity.LogicalName, partyentity.Id);
+                }
+                catch (Exception e)
+                {
+                    tracingservice.Trace(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Scaleable.Xrm.Plugins.CalculateGoalCurrencyPlugin/CalculateGoalCurrency.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaleable.Xrm.Plugins.CalculateGoalCurrencyPlugin
+{
+    public class CalculateGoalCurrency : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            Entity entity;
+            EntityReference reference;
+            decimal num;
+            decimal num2;
+            decimal num3;
+            decimal num4;
+            decimal num5;
+            ColumnSet set;
+            Entity entity2;
+            decimal num6;
+            Exception exception;
+            bool flag;
+            string[] strArray;
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService service = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IOrganizationService service2 = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(new Guid?(context.UserId));
+            if (!context.InputParameters.Contains("Target") || !(context.InputParameters["Target"] is Entity))
+            {
+                return;
+            }
+            else
+            {
+                flag = context.MessageName != "Create";
+                if (!flag)
+                {
+                    try
+                    {
+                        entity = (Entity)context.InputParameters["Target"];
+                        if (entity.LogicalName == "goal")
+                        {
+                            reference = new EntityReference();
+                            num = 0M;
+                            num2 = 0M;
+                            num3 = 0M;
+                            num4 = 0M;
+                            num5 = 0M;
+                            if (entity.Attributes.Contains("ss_currency"))
+                            {
+                                reference = (EntityReference)entity["ss_currency"];
+                            }
+                            if (entity.Attributes.Contains("ss_targetmoney"))
+                            {
+                                num2 = (decimal)entity["ss_targetmoney"];
+                            }
+                            if (entity.Attributes.Contains("actualmoney"))
+                            {
+                                num3 = ((Money)entity["actualmoney"]).Value;
+                            }
+                            if (entity.Attributes.Contains("inprogressmoney"))
+                            {
+                                num4 = ((Money)entity["inprogressmoney"]).Value;
+                            }
+                            if (entity.Attributes.Contains("computedtargetasoftodaymoney"))
+                            {
+                                num5 = ((Money)entity["computedtargetasoftodaymoney"]).Value;
+                            }
+                            if (reference.Id != Guid.Empty)
+                            {
+                                strArray = new string[] { "exchangerate" };
+                                set = new ColumnSet(strArray);
+                                entity2 = service2.Retrieve("transactioncurrency", reference.Id, set);
+                                service.Trace("after query ", new object[0]);
+                                flag = !entity2.Attributes.Contains("exchangerate");
+                                if (!flag)
+                                {
+                                    num = (decimal)entity2["exchangerate"];
+                                }
+                                num6 = num2 / num;
+                                entity["targetmoney"] = new Money(num6);
+                                entity["ss_actualmoney"] = num3 * num;
+                                entity["ss_inprogressmoney"] = num4 * num;
+                                entity["ss_computedtargetasoftodaymoney"] = num5 * num;
+                            }
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    catch (Exception exception1)
+                    {
+                        exception = exception1;
+                        throw new InvalidPluginExecutionException("An error occurred in the currency calculation plugin.", exception);
+                    }
+                }
+            }
+            if (context.MessageName == "Update")
+            {
+                Entity entity3 = context.PreEntityImages["goalPreimage"];
+                entity = (Entity)context.InputParameters["Target"];
+                flag = entity.LogicalName == "goal";
+                if (flag)
+                {
+                    try
+                    {
+                        reference = new EntityReference();
+                        num = 0M;
+                        num2 = 0M;
+                        num3 = 0M;
+                        num4 = 0M;
+                        num5 = 0M;
+                        if (entity.Attributes.Contains("ss_currency"))
+                        {
+                            reference = (EntityReference)entity["ss_currency"];
+                        }
+                        else if (entity3.Attributes.Contains("ss_currency"))
+                        {
+                            reference = (EntityReference)entity3["ss_currency"];
+                        }
+                        if (entity.Attributes.Contains("ss_targetmoney"))
+                        {
+                            num2 = (decimal)entity["ss_targetmoney"];
+                        }
+                        else if (entity3.Attributes.Contains("ss_targetmoney"))
+                        {
+                            num2 = (decimal)entity3["ss_targetmoney"];
+                        }
+                        if (entity.Attributes.Contains("actualmoney"))
+                        {
+                            num3 = ((Money)entity["actualmoney"]).Value;
+                        }
+                        else if (entity3.Attributes.Contains("actualmoney"))
+                        {
+                            num3 = ((Money)entity3["actualmoney"]).Value;
+                        }
+                        if (entity.Attributes.Contains("inprogressmoney"))
+                        {
+                            num4 = ((Money)entity["inprogressmoney"]).Value;
+                        }
+                        else if (entity3.Attributes.Contains("inprogressmoney"))
+                        {
+                            num4 = ((Money)entity3["inprogressmoney"]).Value;
+                        }
+                        if (entity.Attributes.Contains("computedtargetasoftodaymoney"))
+                        {
+                            num5 = ((Money)entity["computedtargetasoftodaymoney"]).Value;
+                        }
+                        else if (entity3.Attributes.Contains("computedtargetasoftodaymoney"))
+                        {
+                            num5 = ((Money)entity3["computedtargetasoftodaymoney"]).Value;
+                        }
+                        if (reference.Id != Guid.Empty)
+                        {
+                            strArray = new string[] { "exchangerate" };
+                            set = new ColumnSet(strArray);
+                            entity2 = service2.Retrieve("transactioncurrency", reference.Id, set);
+                            if (entity2.Attributes.Contains("exchangerate"))
+                            {
+                                num = (decimal)entity2["exchangerate"];
+                            }
+                            num6 = num2 / num;
+                            entity["targetmoney"] = new Money(num6);
+                            entity["ss_actualmoney"] = num3 * num;
+                            entity["ss_inprogressmoney"] = num4 * num;
+                            entity["ss_computedtargetasoftodaymoney"] = num5 * num;
+                        }
+                    }
+                    catch (Exception exception2)
+                    {
+                        exception = exception2;
+                        throw new InvalidPluginExecutionException("An error occurred in the currency calculation plugin.", exception);
+                    }
+                }
+            }
+        }
+    }
+}
