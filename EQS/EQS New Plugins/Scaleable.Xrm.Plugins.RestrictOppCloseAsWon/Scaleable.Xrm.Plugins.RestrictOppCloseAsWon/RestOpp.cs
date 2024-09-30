1585bfd8e0729c994967f4ad86312c7d275356ce
MuhammadIrfan6005 <Muhammadirfan6005@gmail.com>
2024-09-30 16:29:41 +0500

    change folder structure
    change folder structure

---
+++ EQS/EQS New Plugins/Scaleable.Xrm.Plugins.RestrictOppCloseAsWon/Scaleable.Xrm.Plugins.RestrictOppCloseAsWon/RestOpp.cs
@@ -0,0 +1,130 @@
+﻿using log4net.Plugin;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaleable.Xrm.Plugins.RestrictOppCloseAsWon
+{
+    public class RestOpp : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            #region Standard Initializations
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+
+            IOrganizationServiceFactory factory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            #endregion Standard Initializations
+
+            tracingService.Trace("traincing start");
+            tracingService.Trace("Depth: "+ context.Depth);
+
+            #region plugin start
+            try
+            {
+
+                string stepname = null; 
+                int status =0;
+                Int32 count = 0;
+
+                if (!(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity))
+                    return;
+                Entity preEntity = new Entity();
+                if (context.MessageName == "Update" && context.PreEntityImages.Contains("PreEntity"))
+                {
+                    preEntity = (Entity)context.PreEntityImages["PreEntity"];
+                    tracingService.Trace("Image contains");
+                }
+
+                Entity entity = (Entity)context.InputParameters["Target"];
+                if (entity.LogicalName != "opportunity" && context.MessageName != "Update")
+                    return;
+                Guid opportunityid = entity.Id;
+                tracingService.Trace("ID " + opportunityid.ToString());
+
+                if (entity.Attributes.Contains("statecode"))
+                {
+                    if(entity.Attributes["statecode"] != null)
+                    {
+                        status = entity.GetAttributeValue<OptionSetValue>("statecode").Value;
+                        //status = )entity.Attributes["statecode"];
+                        tracingService.Trace("Status " + status);
+                    }
+                }
+                if(status == 1)
+                {
+                    if (entity.Attributes.Contains("stepname"))
+                        stepname = (String)entity.Attributes["stepname"];
+                    else
+                        stepname = (String)preEntity.Attributes["stepname"];
+                    tracingService.Trace("stepname " + stepname);
+
+
+                    #region Query
+                    var QEannotation_notetext = "%{{Contract--%";
+                    var QEannotation_opportunity_opportunityid = opportunityid.ToString();
+
+                    // Instantiate QueryExpression QEannotation
+                    var QEannotation = new QueryExpression("annotation");
+
+                    // Add columns to QEannotation.ColumnSet
+                    QEannotation.ColumnSet.AddColumns("subject", "notetext", "filename", "annotationid");
+                    QEannotation.AddOrder("subject", OrderType.Ascending);
+
+                    // Define filter QEannotation.Criteria
+                    QEannotation.Criteria.AddCondition("notetext", ConditionOperator.Like, QEannotation_notetext);
+
+                    // Add link-entity QEannotation_opportunity
+                    var QEannotation_opportunity = QEannotation.AddLink("opportunity", "objectid", "opportunityid");
+                    QEannotation_opportunity.EntityAlias = "ag";
+
+                    // Define filter QEannotation_opportunity.LinkCriteria
+                    QEannotation_opportunity.LinkCriteria.AddCondition("opportunityid", ConditionOperator.Equal, QEannotation_opportunity_opportunityid);
+                    #endregion end query 
+
+                    EntityCollection entityCollection = service.RetrieveMultiple(QEannotation);
+                    tracingService.Trace("Notes Count =" + entityCollection.Entities.Count);
+                    count = entityCollection.Entities.Count;
+
+                    if (stepname != "4-Close")
+                    {
+                        tracingService.Trace("Pipeline Phase does not Close");
+                        throw new InvalidPluginExecutionException(OperationStatus.Failed, "Please complete all the Business Stages; ");
+                    }
+                    if (count < 1) 
+                    {
+                        tracingService.Trace("There is no contract");
+                        throw new InvalidPluginExecutionException(OperationStatus.Failed, "Contract is missing, Please upload the Contract; ");
+                    }
+
+                }
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "Please complete all the Business Stages; ")
+                    throw new InvalidPluginExecutionException(OperationStatus.Failed, "Please complete all the Business Stages; ");
+                else if(ex.Message == "Contract is missing, Please upload the Contract; ")
+                    throw new InvalidPluginExecutionException(OperationStatus.Failed, "Contract is missing, Please upload the Contract; ");
+                else
+                    tracingService.Trace("something went other wrong :" + ex.Message);
+            }
+            #endregion
+        }
+    }
+}
---
+++ EQS/EQS New Plugins/Scaleable.Xrm.Plugins.SalesOppProducts copy/SyncProducts.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Scaleable.Xrm.Plugins.SalesOppProducts
+{
+    public class SyncProducts:IPlugin
+
+    {
+        public static decimal licensefeevalue;
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext executioncontext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory servicefactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = servicefactory.CreateOrganizationService(executioncontext.UserId);
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (executioncontext.InputParameters.Contains("Target") && executioncontext.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)executioncontext.InputParameters["Target"];
+
+                try
+                {
+                    if(entity.LogicalName== "opportunity")
+                    {
+                        if(entity.Attributes.Contains("ss_productselectortimestamp"))
+                        {
+                            // Instantiate QueryExpression QEopportunityproduct
+                            var QEopportunityproduct = new QueryExpression("opportunityproduct");
+
+                            // Add columns to QEopportunityproduct.ColumnSet
+                            QEopportunityproduct.ColumnSet.AddColumns("productid", "baseamount", "extendedamount_base", "extendedamount");
+
+                            // Define filter QEopportunityproduct.Criteria
+                            QEopportunityproduct.Criteria.AddCondition("opportunityid", ConditionOperator.Equal, entity.Id.ToString());
+
+                            // Add link-entity QEopportunityproduct_product
+                            var QEopportunityproduct_product = QEopportunityproduct.AddLink("product", "productid", "productid");
+                            QEopportunityproduct_product.EntityAlias = "Product";
+
+                            // Add columns to QEopportunityproduct_product.Columns
+                            QEopportunityproduct_product.Columns.AddColumns("name","ss_relatedlicencefee", "ss_issalesproduct", "producttypecode", "productnumber");
+
+                            // Define filter QEopportunityproduct_product.LinkCriteria
+                            QEopportunityproduct_product.LinkCriteria.AddCondition("ss_issalesproduct", ConditionOperator.Equal, true);
+                            QEopportunityproduct_product.LinkCriteria.AddCondition("producttypecode", ConditionOperator.In, 100000001, 100000003);
+
+
+                            // enitity collection:
+
+                            EntityCollection entityCollection = service.RetrieveMultiple(QEopportunityproduct);
+                            tracingService.Trace("Count" + entityCollection.Entities.Count);
+
+                            DeleteSalesoppProducts(serviceProvider, service, entity.Id.ToString());
+                            foreach (var queryexpdata in entityCollection.Entities)
+                            {
+                                OptionSetValue productTypeCode = (OptionSetValue)((queryexpdata.Attributes["Product.producttypecode"] as AliasedValue).Value);
+                                EntityReference licPro = null;
+                                
+                                if (productTypeCode.Value.ToString() == "100000003")
+                                {
+                                    EntityReference productid = (EntityReference)queryexpdata.Attributes["productid"];
+                                   // tracingService.Trace("productid :  is" + productid.Id.ToString());
+
+
+                                    Money extendedamount = (Money)queryexpdata.Attributes["extendedamount"];
+                                    //tracingService.Trace("Extended ammount is : " + extendedamount.Value.ToString());
+                                    Money licenseFee = new Money(0);
+
+      #region ss_JawaD Changes
+
+                                    //EntityReference opProdCur = queryexpdata.GetAttributeValue<EntityReference>("transactioncurrencyid");
+                                    EntityReference opProdCur = queryexpdata.Attributes["transactioncurrencyid"] as EntityReference;
+                                    //currencyLookUp = new EntityReference(opProdCur.LogicalName, opProdCur.Id);
+                                    tracingService.Trace("Currency" + opProdCur.Id);
+                                    //entity.Attributes["new_preowner"] = prLookUp;
+
+      #endregion ss_JawaD Changes
+
+
+                                    if (queryexpdata.Attributes.Contains("Product.ss_relatedlicencefee"))
+                                    {
+                                        AliasedValue relatedlicensefee = ((queryexpdata.Attributes["Product.ss_relatedlicencefee"] as AliasedValue));
+                                        licPro = (EntityReference)relatedlicensefee.Value;
+                                    //    tracingService.Trace("relatedlicensefee :  is" + licPro.Id.ToString());
+
+                                        Entity liscenseProd = (Entity)entityCollection.Entities.Where(x => x.GetAttributeValue<EntityReference>("productid")?.Id.ToString().Trim().ToLower() == licPro?.Id.ToString().Trim().ToLower()).FirstOrDefault();
+                                        //_tracingService.Trace("liscenseProd :is  " + liscenseProd.ToString());
+                                        if (liscenseProd != null)
+                                        {
+                                            licenseFee = (Money)liscenseProd.Attributes["extendedamount"];
+                                        }
+                                    }
+                                
+
+                                    // Create the Object of Sales Opportunity Products
+                                    if (licenseFee == null)
+                                        licenseFee = new Money(0);
+                                    if (extendedamount == null)
+                                        extendedamount = new Money(0);
+
+                                    Entity salesoppProductentity = new Entity("ss_salesopportunityproducts");
+                                   
+                                    salesoppProductentity.Attributes["ss_opportunity"] =new EntityReference("opportunity",entity.Id);
+                                    salesoppProductentity.Attributes["ss_product"] = productid;
+
+#region ss_JawaD Changes
+
+                                    //salesoppProductentity.Attributes["transactioncurrencyid"] = new EntityReference("currency", opProdCur.Id);
+                                    if (opProdCur != null){ 
+                                    salesoppProductentity.Attributes["transactioncurrencyid"] = opProdCur;
+                                    }
+#endregion end ss_JawaD Changes
+                                    salesoppProductentity.Attributes["ss_licensefee"] = licenseFee;
+                                    salesoppProductentity.Attributes["ss_setupfee"] = extendedamount;
+                                    AliasedValue name= queryexpdata.Attributes["Product.name"] as AliasedValue;
+                                    salesoppProductentity.Attributes["ss_name"] = name.Value;
+                                   service.Create(salesoppProductentity);
+
+                                }
+                            }
+                            
+                        }
+
+                    }
+
+                }
+                catch(FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("There is an error in Organization service" + ex);
+                }
+
+            }
+        }
+
+        public void DeleteSalesoppProducts(IServiceProvider serviceProvider, IOrganizationService service, string oppId)
+        {
+            
+            // Instantiate QueryExpression QEss_salesopportunityproducts
+            var QEss_salesopportunityproducts = new QueryExpression("ss_salesopportunityproducts");
+
+            // Add columns to QEss_salesopportunityproducts.ColumnSet
+            QEss_salesopportunityproducts.ColumnSet.AddColumns("ss_salesopportunityproductsid");
+            // Define filter QEss_salesopportunityproducts.Criteria
+            QEss_salesopportunityproducts.Criteria.AddCondition("ss_opportunity", ConditionOperator.Equal, oppId);
+
+            EntityCollection entitycollection = service.RetrieveMultiple(QEss_salesopportunityproducts);
+            foreach(var queryexpression in entitycollection.Entities)
+            {
+                service.Delete(queryexpression.LogicalName, queryexpression.Id);
+            }
+
+        }
+    }
+}
